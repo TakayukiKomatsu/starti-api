bd024ebb7cc8fc2bf2ea45510e833b26d238515f
Takayuki Komatsu <komatsutakayuki235@live.com>
2021-12-16 14:18:01 -0300

    feat: add Product route for listing all products

---
+++ app/Controllers/Http/ProductsController.ts
@@ -0,0 +1,21 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Product from 'App/Models/Product'
+
+export default class ProductsController {
+  public async index({}: HttpContextContract) {
+    const products = await Product.all()
+    return products
+  }
+
+  public async create({}: HttpContextContract) {}
+
+  public async store({}: HttpContextContract) {}
+
+  public async show({}: HttpContextContract) {}
+
+  public async edit({}: HttpContextContract) {}
+
+  public async update({}: HttpContextContract) {}
+
+  public async destroy({}: HttpContextContract) {}
+}
